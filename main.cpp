ab5f919b89b46f5ae9b8701bfd1f9542899ff458
sky <1574480751@qq.com>
2024-06-03 15:08:49 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iosfwd>
+
+void sort(int arr[], int n)
+{
+    
+}
+
+
+int main()
+{
+    int arr[] = {1, 22, 31, 14, 5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, size);
+
+    return 0;
+
+}
